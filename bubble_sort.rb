5e4726ae7945f6c6e0fc45c9e5791ff12aa641ca
Ethee <jakemmattern@gmail.com>
2020-09-27 10:02:10 -0400

    Edit for style

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,11 @@
-def bubble_sort( arr )
-  iteration = ( arr.length / 2 ) + 1
+def bubble_sort(arr)
+  iteration = (arr.length / 2) + 1
   iteration.times do
     arr.each do |element|
       cur_index = arr.index(element)
-      if ( cur_index == arr.length - 1 )
+      if cur_index == arr.length - 1
         break
-      elsif ( element > arr[cur_index + 1] )
+      elsif element > arr[cur_index + 1]
         placeholder = element
         arr[cur_index] = arr[cur_index + 1]
         arr[cur_index + 1] = placeholder
